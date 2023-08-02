bbab8baa2db814952f6942e9f06a573c735688dd
mohamed rhanmi <mohamed.rhanmi@gmail.com>
2023-08-02 17:22:38 +0200

    Add Prisma db

--- src/app/api/code/route.ts
+++ src/app/api/code/route.ts
@@ -2,14 +2,21 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+import { checkSubscription } from "@/lib/subscription";
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
-const openAi = new OpenAIApi(configuration);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 const instructionMessage: ChatCompletionRequestMessage = {
   role: "system",
   content:
-    "You are a code generator. You must answer only in markdown snippets. Use code comments for explanations.",
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
 };
+
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
@@ -19,17 +26,35 @@ export async function POST(req: Request) {
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
     if (!configuration.apiKey) {
-      return new NextResponse("OpenAi API Key not configured", { status: 500 });
-    }
-    if (!messages) {
-      return new NextResponse("Message are required", { status: 400 });
+      return new NextResponse("OpenAI API Key not configured.", {
+        status: 500,
+      });
     }
 
-    const response = await openAi.createChatCompletion({
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
+
+    if (!freeTrial && !isPro) {
+      return new NextResponse(
+        "Free trial has expired. Please upgrade to pro.",
+        { status: 403 }
+      );
+    }
+
+    const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [instructionMessage, ...messages],
     });
+
+    if (!isPro) {
+      await incrementApiLimit();
+    }
 
     return NextResponse.json(response.data.choices[0].message);
   } catch (error) {
