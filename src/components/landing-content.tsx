c70e6156019e92cafde9467fcfd5bf325aecc4a3
mohamed rhanmi <mohamed.rhanmi@gmail.com>
2023-08-24 17:44:50 +0200

    Responsive landing page

--- src/components/landing-content.tsx
+++ src/components/landing-content.tsx
@@ -67,18 +67,18 @@ const features = [
 export const LandingContent = () => {
 
   return (
-    <div className="px-12">
-      <div className="bg-[#000814]/70 backdrop-blur-md flex flex-col gap-4 items-center rounded-md py-8">
-        <h2 className="text-4xl font-bold text-slate-50">Our features</h2>
-        <div className="grid grid-cols-3 gap-2 px-8">
+    <div className="px-4 md:px-8 :px-12">
+      <div className="bg-[#000814]/70 backdrop-blur-md flex flex-col  gap-2 md:gap-4 items-center rounded-md py-6 md:py-8">
+        <h2 className="text-3xl md:text-4xl font-bold text-slate-50">Our features</h2>
+        <div className="flex flex-col md:grid md:grid-cols-3 gap-2 px-8">
         {features.map((ft, index)=>(<Card key={index} className="bg-[#000814] border-none flex flex-col p-2">
-         <CardHeader className="flex flex-row items-center  gap-2">
-          <div className="w-8 h-8 text-white flex items-center justify-center bg-gradient-to-br from-purple-400 to-purple-900 rounded-md">
+         <CardHeader className="flex flex-row items-center  gap-1 md:gap-3 ">
+          <div className="w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8 text-white flex items-center justify-center bg-gradient-to-br from-purple-400 to-purple-900 rounded-md p-1">
           {ft.icon}
          </div>
-          <CardTitle className="text-white ">{ft.title}</CardTitle>         
+          <CardTitle className="text-white text-xl lg:text-2xl p-0">{ft.title}</CardTitle>         
          </CardHeader> 
-          <CardContent className="text-white">{ft.description}</CardContent></Card>))}
+          <CardContent className="text-white text-sm lg:text-md">{ft.description}</CardContent></Card>))}
         </div>
         </div>
      
