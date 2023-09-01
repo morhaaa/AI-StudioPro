e6ec3828e7e821efb5cd5cb7eeef15ed2985d84c
mohamed rhanmi <106605846+morhaaa@users.noreply.github.com>
2023-09-01 17:58:58 +0200

    Create user tab

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { FreeCounter } from "./free-Counter";
 import { tools } from "@/constants";
+import UserTab from "./user-tab";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -18,39 +19,44 @@ interface ISidebar {
 const Sidebar: React.FC<ISidebar> = ({ apiLimitCount, isPro }) => {
   const pathname = usePathname();
   return (
-    <div className="h-full w-full flex flex-col">
-      <Link href={{ pathname: "/dashboard" }}>
-        {" "}
-        <p
-          className={cn(
-            "text-white font-semibold flex justify-center py-6 md:text-lg lg:text-xl xl:text-2xl ",
-            montserrat.className
-          )}
-        >
-          AI STUDIO PRO
-        </p>
-      </Link>
-      <nav className="px-3 py-4 flex flex-col">
-        {tools.map((route, index) => (
-          <Link
-            href={route.href}
-            key={index}
+    <div className="h-full w-full flex flex-col justify-between">
+      <div className="flex flex-col">
+        <Link href={{ pathname: "/dashboard" }}>
+          {" "}
+          <p
             className={cn(
-              "flex justify-start hover:bg-white/10 rounded-lg transition py-2",
-              pathname === route.href
-                ? "text-white bg-white/10"
-                : "text-zinc-400"
+              "text-white font-semibold flex justify-center py-6 md:text-lg lg:text-xl xl:text-2xl ",
+              montserrat.className
             )}
           >
-            <div className="flex gap-2 items-center">
-              {route.icon}
-              <p className="md:text-lg lg:text-xl xl:text-2xl text-white font-medium">
-                {route.title}
-              </p>
-            </div>
-          </Link>
-        ))}
-      </nav>
+            AI STUDIO PRO
+          </p>
+        </Link>
+        <nav className="px-3 py-4 flex flex-col">
+          {tools.map((route, index) => (
+            <Link
+              href={route.href}
+              key={index}
+              className={cn(
+                "flex justify-start hover:bg-white/10 rounded-lg transition py-2",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
+            >
+              <div className="flex gap-2 items-center">
+                {route.icon}
+                <p className="md:text-lg lg:text-xl xl:text-2xl text-white font-medium">
+                  {route.title}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </nav>
+      </div>
+      <div className=" px-2 pb-4">
+        <UserTab />
+      </div>
     </div>
   );
   {
---
+++ src/components/user-tab.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const UserTab: React.FC<Props> = ({}) => {
+  return (
+    <div className="bg-gradient-to-br from-slate-400 to-slate-700 rounded-md">
+      user-tab
+    </div>
+  );
+};
+
+export default UserTab;
